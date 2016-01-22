cad5308fa56d5bac29dec266aa2ee7ac2d3ebf0f
Anthony Garvan <anthony.garvan@gsa.gov>
2016-01-22 17:00:10 -0600

    adding more test coverage

--- spec/frontEndSpec.js
+++ spec/frontEndSpec.js
@@ -22,6 +22,10 @@ describe('todoCtrl', function() {
 
       $httpBackend.when('GET', '/api/get-users')
                       .respond({ users: [{earliestDueDate: new Date().toString()}] });
+
+      $httpBackend.when('GET', '/api/complete-item')
+                      .respond({ updatedItemCount: 1 });
+                                          
        // Get hold of a scope (i.e. the root scope)
        $rootScope = $injector.get('$rootScope');
        // The $controller service is used to create instances of controllers
@@ -32,11 +36,18 @@ describe('todoCtrl', function() {
        };
      }));
 
-    it('testTrafficLight', function() {
+    it('shows trafficLight properly', function() {
       var controller = createController();
       $httpBackend.flush();
       expect($rootScope.getTrafficLight(0)).toEqual('redLight');
       expect($rootScope.getTrafficLight(2)).toEqual('yellowLight');
       expect($rootScope.getTrafficLight(3)).toEqual('greenLight');
+    });
+
+    it('marks item complete', function() {
+      var controller = createController();
+      $httpBackend.flush();
+      var item = {itemId: '1', checklistName: 'test', _id: '123', timestamp: 1234};
+      $rootScope.markDone(item);
     });
 });

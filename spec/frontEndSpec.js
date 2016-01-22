fb6cf40597dbb435d25468b8fe41790ebf4b63a5
Anthony Garvan <anthony.garvan@gsa.gov>
2016-01-22 16:42:25 -0600

    starting to add front end tests

--- spec/frontEndSpec.js
+++ spec/frontEndSpec.js
@@ -3,18 +3,40 @@ describe('todoCtrl', function() {
 
   var $controller;
 
-  beforeEach(inject(function(_$controller_){
-    // The injector unwraps the underscores (_) from around the parameter names when matching
-    $controller = _$controller_;
-  }));
+  //beforeEach(inject(function(_$controller_){
+  //  $controller = _$controller_;
+  //}));
 
-  describe('$scope.grade', function() {
+    beforeEach(inject(function($injector) {
+       // Set up the mock http service responses
+       $httpBackend = $injector.get('$httpBackend');
+       // backend definition common for all tests
+       $httpBackend.when('GET', '/api/')
+                      .respond({ message: 'hooray! welcome to the api!' });
+
+       $httpBackend.when('GET', '/api/get-checklists')
+                      .respond({ checklists: [{}] });
+
+      $httpBackend.when('GET', '/api/get-items')
+                      .respond({ items: [{dueDate: new Date().toString(), description: 'test'}] });
+
+      $httpBackend.when('GET', '/api/get-users')
+                      .respond({ users: [{earliestDueDate: new Date().toString()}] });
+       // Get hold of a scope (i.e. the root scope)
+       $rootScope = $injector.get('$rootScope');
+       // The $controller service is used to create instances of controllers
+       var $controller = $injector.get('$controller');
+
+       createController = function() {
+         return $controller('todoCtrl', {'$scope' : $rootScope });
+       };
+     }));
+
     it('testTrafficLight', function() {
-      var $scope = {};
-      var controller = $controller('todoCtrl', { $scope: $scope });
-      expect($scope.getTrafficLight(0)).toEqual('redLight');
-      expect($scope.getTrafficLight(2)).toEqual('yellowLight');
-      expect($scope.getTrafficLight(3)).toEqual('greenLight');
+      var controller = createController();
+      $httpBackend.flush();
+      expect($rootScope.getTrafficLight(0)).toEqual('redLight');
+      expect($rootScope.getTrafficLight(2)).toEqual('yellowLight');
+      expect($rootScope.getTrafficLight(3)).toEqual('greenLight');
     });
-  });
 });

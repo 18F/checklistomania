f77dbb396de8ca4bb5df68cb16cca7c61c357c23
James Seppi <james.seppi@gmail.com>
2016-05-10 23:25:04 -0500

    eslint cleanup

--- karma.conf.js
+++ karma.conf.js
@@ -1,47 +1,47 @@
-module.exports = function(config){
+module.exports = function configKarma(config) {
   config.set({
 
-    basePath : './',
+    basePath: './',
 
-    files : [
+    files: [
       'public/bower_components/angular/angular.js',
       'public/bower_components/angular-mocks/angular-mocks.js',
       'public/bower_components/angular-material/angular-material.js',
-      "public/bower_components/angular-animate/angular-animate.min.js",
-      "public/bower_components/angular-aria/angular-aria.min.js",
-      "public/bower_components/angular-messages/angular-messages.min.js",
+      'public/bower_components/angular-animate/angular-animate.min.js',
+      'public/bower_components/angular-aria/angular-aria.min.js',
+      'public/bower_components/angular-messages/angular-messages.min.js',
       'private/js/app.js',
       'spec/frontEndSpec.js'
     ],
 
-    autoWatch : true,
+    autoWatch: true,
 
     frameworks: ['jasmine'],
 
-    browsers : ['PhantomJS'],
+    browsers: ['PhantomJS'],
 
     reporters: ['progress', 'coverage'],
 
     preprocessors: {
       'private/js/app.js': ['coverage']
     },
-    
-    coverageReporter: {
-       type : 'lcov',
-       dir : './coverage',
-       subdir: 'phantomjs'
-     },
-    
-    plugins : [
-            'karma-chrome-launcher',
-            'karma-firefox-launcher',
-            'karma-phantomjs-launcher',
-            'karma-jasmine',
-            'karma-junit-reporter',
-            'karma-coverage'
-            ],
 
-    junitReporter : {
+    coverageReporter: {
+      type: 'lcov',
+      dir: './coverage',
+      subdir: 'phantomjs'
+    },
+
+    plugins: [
+      'karma-chrome-launcher',
+      'karma-firefox-launcher',
+      'karma-phantomjs-launcher',
+      'karma-jasmine',
+      'karma-junit-reporter',
+      'karma-coverage'
+    ],
+
+    junitReporter: {
       outputFile: 'test_out/unit.xml',
       suite: 'unit'
     }

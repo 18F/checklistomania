74381b263aea6ea6cf09b654c8604834a3ef331a
James Seppi <james.seppi@gmail.com>
2016-05-13 11:58:01 -0500

    lint app.js
    - still needs a light refactor of the getApp method to not shadow variables

--- app.js
+++ app.js
@@ -1,35 +1,88 @@
 
 var express = require('express');
-var util = require('util');
 var session = require('express-session');
 var bodyParser = require('body-parser');
+var http = require('http');
 var methodOverride = require('method-override');
-var api = require('./api/api.js');
-var request = require('request');
+var passport = require('passport');
+var GitHubStrategy = require('passport-github2').Strategy;
 
-var getApp = function(passport, GitHubStrategy, github) {
+var api = require('./api/api.js');
+var github = require('./api/github.js').github;
+
+var serverApp;
+var server;
+
+var getApp = function (passport, GitHubStrategy, github) { // eslint-disable-line no-shadow
+  var app = express();
   api.setGithub(github);
-  passport.serializeUser(function(user, done) {
+
+  passport.serializeUser(function (user, done) {
     done(null, user);
   });
 
-  passport.deserializeUser(function(obj, done) {
+  passport.deserializeUser(function (obj, done) {
     done(null, obj);
   });
 
   passport.use(new GitHubStrategy({
-      clientID: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      callbackURL: process.env.GITHUB_CALLBACK_URL || "http://localhost:3000/auth/callback"
-    },
-    function(accessToken, refreshToken, profile, done) {
-      process.nextTick(function () {
-          return done(null, profile);
-      });
-    }
-  ));
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: process.env.GITHUB_CALLBACK_URL || 'http://localhost:3000/auth/callback'
+  }, function (accessToken, refreshToken, profile, done) {
+    process.nextTick(function () {
+      return done(null, profile);
+    });
+  }));
 
-  var app = express();
+  function ensureAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+      next();
+      return;
+    }
+    res.redirect('/auth');
+  }
+
+  function ensureGithubOrg(req, res, next) {
+    api.db.collection('users').findOne({ username: req.user.username },
+      function (err, user) {
+        if (user) {
+          next();
+        } else {
+          github.orgs.getFromUser({ user: req.user.username },
+            function (getFromUserErr, orgs) {
+              var inOrg = false;
+              orgs.forEach(function (org) {
+                if (org.login === process.env.GITHUB_ORG) {
+                  inOrg = true;
+                }
+              });
+              if (inOrg) {
+                next();
+              } else {
+                res.redirect('/not-authorized.html');
+              }
+            });
+        }
+      });
+  }
+
+  function addToUsers(req, res, next) {
+    api.db.collection('users').findOne({ username: req.user.username },
+      function (err, user) {
+        var newUser;
+        if (!user) {
+          newUser = {
+            username: req.user.username,
+            earliestDueDate: new Date().setYear(3000),
+            fullName: req.user._json.name,
+            imgUrl: req.user._json.avatar_url
+          };
+          api.db.collection('users').insert(newUser);
+        }
+        next();
+      });
+  }
 
   app.use(bodyParser());
   app.use(bodyParser.json());
@@ -38,77 +91,32 @@ var getApp = function(passport, GitHubStrategy, github) {
   app.use(passport.initialize());
   app.use(passport.session());
 
-  app.use( "/private", [ ensureAuthenticated, ensureGithubOrg, addToUsers, express.static( "private" ) ] );
+  app.use('/private', [ensureAuthenticated, ensureGithubOrg,
+    addToUsers, express.static('private')]);
   app.use('/api', [ensureAuthenticated, ensureGithubOrg, addToUsers, api.router]);
 
-  app.use( "/", express.static( "public" ) );
+  app.use('/', express.static('public'));
 
-  app.get('/auth',
-    passport.authenticate('github', { scope: [ 'user:email' ] }),
-    function(req, res){
-    });
+  app.get('/auth', passport.authenticate('github', { scope: ['user:email'] }), function () {});
 
   app.get('/auth/callback',
     passport.authenticate('github', { failureRedirect: '/login' }),
-    function(req, res) {
+    function (req, res) {
       res.redirect('/private/index.html');
     });
 
-  app.get('/logout', function(req, res){
+  app.get('/logout', function (req, res) {
     req.logout();
     res.redirect('/');
   });
 
-  function ensureAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) { return next(); }
-    res.redirect('/auth');
-  };
-
-  function ensureGithubOrg(req, res, next) {
-    api.db.collection('users').findOne({username: req.user.username},
-      function(err, user) {
-          if(user) {next()}
-          else {
-            github.orgs.getFromUser({user: req.user.username},
-              function(err, orgs) {
-                  var inOrg = false;
-                    orgs.forEach(function(org) {
-                      if(org.login === process.env.GITHUB_ORG){
-                        inOrg = true;
-                      }
-                    });
-                  if(inOrg) {next();}
-                  else {res.redirect('/not-authorized.html');}
-              });
-          };
-  });
-  }
-
-  function addToUsers(req, res, next) {
-    api.db.collection('users').findOne({username: req.user.username},
-      function(err, user) {
-        if(!user) {
-          var user = {username: req.user.username,
-            earliestDueDate: new Date().setYear(3000),
-            fullName: req.user._json.name,
-            imgUrl: req.user._json.avatar_url};
-          api.db.collection('users').insert(user)
-        }
-        next();
-      });
-  }
-
   return app;
-}
+};
 
-module.exports['getApp'] = getApp;
-
-var passport = require('passport');
-var GitHubStrategy = require('passport-github2').Strategy;
-var github = require('./api/github.js').github;
+module.exports.getApp = getApp;
 
 if (require.main === module) {
-  var app = getApp(passport, GitHubStrategy, github);
-  var server = require('http').createServer(app);
+  serverApp = getApp(passport, GitHubStrategy, github);
+  server = http.createServer(serverApp);
   server.listen(process.env.PORT || 3000, function () {});
 }

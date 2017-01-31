4e91f0b9206e7d16ce7f5f3eb56109649285d5ba
James Seppi <james.seppi@gsa.gov>
2017-01-31 16:30:13 -0600

    finish tests for env module

--- spec/backend/envSpec.js
+++ spec/backend/envSpec.js
@@ -1,6 +1,12 @@
 /* global describe it expect */
 var env = require('../../lib/env');
 
+function makeVcapServicesEnv(vcapServices) {
+  return {
+    VCAP_SERVICES: JSON.stringify(vcapServices)
+  };
+}
+
 describe('env properly loads config values', function () {
   it('has default MONGODB_URI', function (done) {
     var e = env.load({});
@@ -8,10 +14,54 @@ describe('env properly loads config values', function () {
     done();
   });
 
+  it('defaults other required values to null', function (done) {
+    var e = env.load({});
+    expect(e.GITHUB_CLIENT_ID).toBeNull();
+    expect(e.GITHUB_CLIENT_SECRET).toBeNull();
+    expect(e.GITHUB_ORG).toBeNull();
+    expect(e.SESSION_SECRET).toBeNull();
+    done();
+  });
+
+  it('loads from the specified env', function (done) {
+    var e = env.load({
+      SOME_ENV_VAR: 'whatever'
+    });
+    expect(e.SOME_ENV_VAR).toBe('whatever');
+    done();
+  });
+
+  it('loads from process.env by default', function (done) {
+    var e;
+    process.env = {
+      ENV_VAR_IN_PROCESS_ENV: 'whatever'
+    };
+    e = env.load();
+    expect(e.ENV_VAR_IN_PROCESS_ENV).toBe('whatever');
+    done();
+  });
+
   it('loads from VCAP_SERVICES', function (done) {
-    // TODO: load mock VCAP_SERVICES
-    // var vcapString = JSON.stringify()
-    // var = env.load()
+    var e = env.load(makeVcapServicesEnv({
+      'user-provided': [{
+        credentials: {
+          GITHUB_CLIENT_ID: 'clientId',
+          GITHUB_CLIENT_SECRET: 'clientSecret',
+          GITHUB_ORG: 'org',
+          SESSION_SECRET: 'sessionSecret'
+        }
+      }],
+      'mongodb26-swarm': [{
+        credentials: {
+          uri: 'mongodb://whatever'
+        }
+      }]
+    }));
+    expect(e.GITHUB_CLIENT_ID).toBe('clientId');
+    expect(e.GITHUB_CLIENT_SECRET).toBe('clientSecret');
+    expect(e.GITHUB_ORG).toBe('org');
+    expect(e.SESSION_SECRET).toBe('sessionSecret');
+    expect(e.MONGODB_URI).toBe('mongodb://whatever');
     done();
   });
 });
